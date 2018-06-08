1cb30cef51be3771ca445cde29a2281535a54b04
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-06-07 18:06:26 -0700

    Remove babel

--- webpack.config.js
+++ webpack.config.js
@@ -16,16 +16,6 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      },
       { test: /\.pug$/, use: "pug-loader" },
       {
         test: /\.scss$/,
